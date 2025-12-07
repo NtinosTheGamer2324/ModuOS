bb3701c5d9d3a891e0a362a3afbbf031cfc732d4
NtinosTheGamer2324 <konstantinosgiann12@gmail.com>
2025-12-07 15:04:58 +0200

    Removed Useless comments

--- include/moduos/arch/AMD64/interrupts/timer.h
+++ include/moduos/arch/AMD64/interrupts/timer.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 void pit_init(uint32_t frequency);
-void timer_irq_handler(void);  // ADD THIS
-uint64_t get_system_ticks(void);  // ADD THIS
+void timer_irq_handler(void);  
+uint64_t get_system_ticks(void);  
 
 #endif
