c70b7c30f24db17f118eaf8961d6d591022cb812
NtinosTheGamer2324 <konstantinosgiann12@gmail.com>
2025-12-23 20:53:45 +0200

    source: update src/arch/AMD64/boot/header.asm

--- src/arch/AMD64/boot/header.asm
+++ src/arch/AMD64/boot/header.asm
@@ -9,7 +9,19 @@ header_start:
 	; checksum
 	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 
+	; framebuffer request tag (type=5)
+	; NOTE: Multiboot2 header tags must be 8-byte aligned and size must be a multiple of 8.
+	align 8
+	dw 5
+	dw 0
+	dd 24
+	dd 1024
+	dd 768
+	dd 32
+	dd 0 ; padding
+
 	; end tag
+	align 8
 	dw 0
 	dw 0
 	dd 8
