3a7125eea30f3a9312dce7cfbdc289ef5b9ee9ac
NtinosTheGamer2324 <konstantinosgiann12@gmail.com>
2025-12-23 20:53:31 +0200

    input: update ps2 (include/moduos/drivers/input/ps2/ports.h)

--- include/moduos/drivers/input/ps2/ports.h
+++ include/moduos/drivers/input/ps2/ports.h
@@ -3,10 +3,13 @@
 #define PS2_COMMAND_PORT    0x64
 
 // PS/2 Controller Commands
-#define PS2_CMD_READ_CONFIG     0x20
-#define PS2_CMD_WRITE_CONFIG    0x60
-#define PS2_CMD_DISABLE_PORT1   0xAD
-#define PS2_CMD_ENABLE_PORT1    0xAE
+#define PS2_CMD_READ_CONFIG      0x20
+#define PS2_CMD_WRITE_CONFIG     0x60
+#define PS2_CMD_DISABLE_PORT1    0xAD
+#define PS2_CMD_ENABLE_PORT1     0xAE
+#define PS2_CMD_DISABLE_PORT2    0xA7
+#define PS2_CMD_ENABLE_PORT2     0xA8
+#define PS2_CMD_WRITE_TO_PORT2   0xD4
 
 // PS/2 Status Register Bits (read from 0x64)
 #define PS2_STATUS_OUTPUT_BUFFER 0x01
