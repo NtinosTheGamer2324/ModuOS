0aa1ce92dc97c99cdd19f48f76d86c60c14ef181
NtinosTheGamer2324 <konstantinosgiann12@gmail.com>
2025-12-23 20:53:43 +0200

    source: update include/moduos/kernel/syscall/syscall.h

--- include/moduos/kernel/syscall/syscall.h
+++ include/moduos/kernel/syscall/syscall.h
@@ -4,39 +4,15 @@
 #include <stdint.h>
 #include <stddef.h>
 #include "moduos/kernel/md64api.h"
+#include "moduos/fs/fd.h"  // for off_t
 
 #ifndef _SSIZE_T_DEFINED
 #define _SSIZE_T_DEFINED
 typedef long ssize_t;
 #endif
 
-// System call numbers
-#define SYS_EXIT        0
-#define SYS_FORK        1
-#define SYS_READ        2
-#define SYS_WRITE       3
-#define SYS_OPEN        4
-#define SYS_CLOSE       5
-#define SYS_WAIT        6
-#define SYS_EXEC        7
-#define SYS_GETPID      8
-#define SYS_GETPPID     9
-#define SYS_SLEEP       10
-#define SYS_YIELD       11
-#define SYS_MALLOC      12
-#define SYS_FREE        13
-#define SYS_SBRK        14
-#define SYS_KILL        15
-#define SYS_TIME        16
-#define SYS_CHDIR       17
-#define SYS_GETCWD      18
-#define SYS_STAT        19
-#define SYS_MKDIR       20
-#define SYS_RMDIR       21
-#define SYS_UNLINK      22
-#define SYS_INPUT       23
-#define SYS_SSTATS      24
-#define SYS_WRITEFILE   30
+// System call numbers (shared with userspace)
+#include "moduos/kernel/syscall/syscall_numbers.h"
 
 
 // System call handler
@@ -63,8 +39,24 @@ void sys_free(void *ptr);
 void* sys_sbrk(intptr_t increment);
 int sys_kill(int pid, int sig);
 uint64_t sys_time(void);
-char* sys_input(void);
+ssize_t sys_input(char *buf, size_t max_len);
+
+// FS/syscalls
+int sys_chdir(const char *path);
+char* sys_getcwd(char *buf, size_t size);
+int sys_stat(const char *path, void *out_info, size_t out_size);
+off_t sys_lseek(int fd, off_t offset, int whence);
+int sys_mkdir(const char *path);
+int sys_rmdir(const char *path);
+int sys_unlink(const char *path);
+
+int sys_opendir(const char *path);
+int sys_readdir(int fd, char *name_buf, size_t buf_size, int *is_dir, uint32_t *size);
+int sys_closedir(int fd);
 
 md64api_sysinfo_data* sys_get_sysinfo(void);  // Changed to return pointer
+
+// VGA / Console
+// Note: currently implemented directly in syscall_handler() and VGA driver.
 
 #endif
